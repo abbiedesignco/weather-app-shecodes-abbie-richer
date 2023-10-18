3e040e5fe199c7baae62d685e6fdd64500e15011
abbiedesignco <abbier.design@gmail.com>
2023-10-18 17:18:29 -0400

    Added JS codes in JS file

---
+++ src/script.js
@@ -0,0 +1,73 @@
+//Search City
+function searchCity(city) {
+  let key = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  axios.get(url).then(showCurrentLocation);
+}
+
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search-button");
+  searchCity(cityInput.value);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+//Current Location
+function searchLocation(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showCurrentLocation);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentLocation = document.querySelector("#city");
+currentLocation.addEventListener("click", getCurrentLocation);
+searchCity("New York");
+
+//Current Day and Time
+function todayDate() {
+  let currentTime = new Date();
+  let hours = currentTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[currentTime.getDay()];
+  let hour = currentTime.getHours();
+  let minute = currentTime.getMinutes();
+
+  let currentDate = `${day} ${hour}:${minute}`;
+  return currentDate;
+}
+
+let today = document.querySelector("#dateTime");
+today.innerHTML = todayDate();
+
+//Current Weather elements
+function showCurrentLocation(response) {
+  let humidity = document.querySelector(".humidity");
+  humidity.innerHTML = response.data.main.humidity;
+
+  let wind = document.querySelector(".wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
